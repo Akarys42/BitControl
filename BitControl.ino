2bd2bb9b0e3d973ca309897e1f62e50b41e4af4a
Akarys42 <44778521+Akarys42@users.noreply.github.com>
2019-08-14 11:10:53 +0200

    Fix bug for FS19

--- BitControl.ino
+++ BitControl.ino
@@ -3,46 +3,50 @@
 
 #include "Joystick.h"
 
-Joystick_ Joystick[3] = {
-  Joystick_(0x03, JOYSTICK_TYPE_JOYSTICK, 24, 0, true, true, false, false, false, false, false, false, false, false, false),
-  Joystick_(0x04, JOYSTICK_TYPE_JOYSTICK, 0, 0, true, true, false, false, false, false, false, false, false, false, false),
-  Joystick_(0x05, JOYSTICK_TYPE_JOYSTICK, 0, 0, true, true, false, false, false, false, false, false, false, false, false)
+Joystick_ Joystick[1] = {
+  Joystick_(0x03, JOYSTICK_TYPE_JOYSTICK, 27, 0, true, true, false, false, false, false, false, false, false, false, false)
 };
 
-//////////////
+//////////////////////////////////////////
 int pin = 3;
 const int line = 6;
 const int column = 4;
 const int joystick = 3;
-//////////////
+const int deadzone = 20;
+const int joystickButton[3] = {24, 25, 26};
+const int d  = 50;
+//////////////////////////////////////////
 
+const int deadzoneMin = 512 - deadzone;
+const int deadzoneMax = 512 + deadzone;
 int linePin[line];
 int columnPin[column];
 int pinJoystick = 0;
+int x;
+int y;
+bool lastState[joystick];
 
 void setup() {
-  for(int l = 0; l < line; l++) {
+  for (int l = 0; l < line; l++) {
     pinMode(pin, OUTPUT);
     linePin[l] = pin;
     pin++;
   }
 
-  for(int c = 0; c < column; c++) {
+  for (int c = 0; c < column; c++) {
     pinMode(pin, INPUT);
     columnPin[c] = pin;
     pin++;
   }
 
-  for(int j = 0; j < joystick; j++) {
-    Joystick[j].begin();
-  }
+  Joystick[0].begin(true);
 }
 
 void loop() {
-  for(int k = 0; k < line; k++) {
+  for (int k = 0; k < line; k++) {
     digitalWrite(linePin[k], HIGH);
-    for(int v = 0; v < column; v++) {
-      if(digitalRead(columnPin[v]) == HIGH) {
+    for (int v = 0; v < column; v++) {
+      if (digitalRead(columnPin[v]) == HIGH) {
         Joystick[0].pressButton(getButtonId(k, v));
       }
       else {
@@ -52,11 +56,33 @@ void loop() {
     digitalWrite(linePin[k], LOW);
   }
 
-  for(int c = 0; c < joystick; c++) {
-    Joystick[c].setXAxis(analogRead(pinJoystick) * -1 + 1024 );
+  Joystick[0].sendState();
+
+  for (int c = 0; c < joystick; c++) {
+    x = analogRead(pinJoystick) * -1 + 1024;
     pinJoystick++;
-    Joystick[c].setYAxis(analogRead(pinJoystick));
+    y = analogRead(pinJoystick);
     pinJoystick++;
+    if (isInDeadzone(x) == false || isInDeadzone(y) == false) {
+      Joystick[0].pressButton(joystickButton[c]);
+      delay(d);
+      Joystick[0].sendState();
+      Joystick[0].setXAxis(x);
+      Joystick[0].setYAxis(y);
+      Joystick[0].sendState();
+      lastState[c] = true;
+    }
+    else {
+      if (lastState[c] == true) {
+        Joystick[0].setXAxis(512);
+        Joystick[0].setYAxis(512);
+        Joystick[0].sendState();
+        delay(d);
+        Joystick[0].releaseButton(joystickButton[c]);
+        Joystick[0].sendState();
+      }
+      lastState[c] = false;
+    }
   }
   pinJoystick = 0;
 }
@@ -66,4 +92,13 @@ int getButtonId(int k, int v) {
   buttonId = v * line;
   buttonId = buttonId + k;
   return buttonId;
+}
+
+bool isInDeadzone(int pwr) {
+  if (pwr > deadzoneMin && pwr < deadzoneMax) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
